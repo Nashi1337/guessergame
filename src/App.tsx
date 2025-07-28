b7c194310a5d103bd25d5fcd25bf7407ee13699e
Nashi1337 <mcmuffin7796@googlemail.com>
2025-07-28 21:42:08 +0200

    idk

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Routes, Route, HashRouter} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GameListPage from './pages/GameListPage';
 import GamePlayPage from './pages/GamePlayPage';
 import SubmitPage from "./pages/SubmitPage.tsx";
 
 const App = () => (
-    <Router>
+    <HashRouter basename={import.meta.env.BASE_URL}>
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/game/list" element={<GameListPage />} />
             <Route path="/game/:id" element={<GamePlayPage />} />
             <Route path={"/submit"} element={<SubmitPage/>}/>
         </Routes>
-    </Router>
+    </HashRouter>
 );
 
 export default App;
